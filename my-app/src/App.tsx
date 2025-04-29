870b706f4ba8fb14e6d5e3d6c764b328545befbb
urmatgit <urmat2g@gmail.com>
2025-04-29 16:37:25 +0300

    все пункты (1,2,Задача со звездочкой)

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
+import { Suspense } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyComponent from './components/myComponent'
+import HomeWorkClass from './components/myComponent'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,19 +17,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        */}
+      
+      <div>
+      
+        <HomeWorkClass />
+      
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <MyComponent />
+      
     </>
   )
 }
--- my-app/src/components/myComponent.tsx
+++ my-app/src/components/myComponent.tsx
@@ -1,4 +1,83 @@
-function MyComponent(){
-    return (<p>This is my first component!</p>);
+import React, {Component, use} from "react";
+import { FactItem } from "./FactItem";
+import { fact } from "./fact";
+interface StateData {
+       
+       facts: [];
+       loading: boolean;
+       error: boolean;
+       errorMessage:string;
+       
 }
-export default MyComponent;
+class HomeWorkClass extends Component<{},StateData>{
+    constructor(props:{}) {
+        super(props);
+        this.state={
+            facts: [] ,
+            loading:true,
+            error: false,
+            errorMessage: ""
+
+        }
+     console.log('Constructor: компонент создается');
+    }
+      componentDidMount() {
+    console.log('componentDidMount: компонент был смонтирован');
+    this.fetchFacts();
+  }
+    fetchFacts=async ()=>{
+        try{
+            await fetch(`https://catfact.ninja/facts?limit=15`)
+            .then((res)=>
+                {
+                    if (res.status==200)
+                        return res.json();
+                    else 
+                    throw new Error(' status is not 200 ');
+
+                })
+            .then((list)=>{
+                    console.log('List: '+list);
+                    this.setState({
+                        facts: list.data,
+                        loading:false,
+                        error: false,
+                        errorMessage:""
+                    })
+            });
+            
+        }catch (errorMsg){
+            console.error('Ошибка при загрузке данных:', errorMsg);
+            this.setState({ loading: false,error: true,errorMessage:errorMsg.message });
+        }
+    }
+    render() {
+        console.log("render");
+        const { facts, loading,error,errorMessage } = this.state;
+        if(loading){
+            return <div>Loading...</div>
+        }
+        // if (error){
+        //     return <p id="factlistError">{errorMessage}</p>
+        // }
+        return( 
+            <div>
+        <p>This is my first component!</p>
+        <div>
+            <h1>Fast list</h1>
+         
+            <ul id="factlist">
+            { 
+                facts.map((item,index)=>
+                    <li key={index}> {item.fact}</li>
+                ) 
+            }
+            </ul>
+            {error &&<p id="factlistError">{errorMessage}</p>}
+        </div>
+        </div>
+        )
+    }
+}
+ 
+export default HomeWorkClass;
